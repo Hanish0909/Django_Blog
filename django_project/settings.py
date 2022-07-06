faa286f4720e4dd13ece207daf28277a6bba0905
hanish0909 <hanish0909@gmail.com>
2022-07-06 18:40:27 +0530

    updated STATIC_ROOT

--- django_project/settings.py
+++ django_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from importlib.resources import path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,6 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIT_ROOT = Path(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 MEDIA_ROOT = Path(BASE_DIR, 'media')
